cc5380c9b5cad7d57420faf9394a9b3cef14f7e6
Davide-Mancini <mancinidavide73@gmail.com>
2025-10-21 16:19:19 +0200

    bcrypt matches in authservice

--- src/main/java/davidemancini/U5_W3_D1/services/AuthorizationService.java
+++ src/main/java/davidemancini/U5_W3_D1/services/AuthorizationService.java
@@ -6,6 +6,8 @@ import davidemancini.U5_W3_D1.payloads.LoginDTO;
 import davidemancini.U5_W3_D1.security.JWTTools;
 import davidemancini.U5_W3_D1.entities.Dipendente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,10 +16,12 @@ public class AuthorizationService {
     private DipendenteService dipendenteService;
     @Autowired
     private JWTTools jwtTools;
+    @Autowired
+    private PasswordEncoder bCrypt;
 
     public String checkCredentialAndNewToken(LoginDTO body){
         davidemancini.U5_W3_D1.entities.Dipendente dipendenteTrovato = dipendenteService.findByEmail(body.email());
-        if (dipendenteTrovato.getPassword().equals(body.password())){
+        if (bCrypt.matches(body.password(),dipendenteTrovato.getPassword())){
             return jwtTools.createToken(dipendenteTrovato);
 
         }else{
